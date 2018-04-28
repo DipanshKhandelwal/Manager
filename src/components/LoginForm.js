f863aa9b77ffb8241fef9f340c85380c1f9debfd
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-28 18:58:50 +0530

    Add firebase login to Login button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React , {Component} from 'react';
 import { Card, Button, CardSection, Input } from './common';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
     onEmailChange(text) {
@@ -12,6 +12,10 @@ class LoginForm extends Component {
         this.props.passwordChanged(text);
     }
 
+    onButtonPress() {
+        const { email, password } = this.props;
+        this.props.loginUser({ email, password });
+    }
 
     render() {
         return (
@@ -37,6 +41,7 @@ class LoginForm extends Component {
                 <CardSection>
                     <Button
                         text='Login'
+                        onPress={this.onButtonPress.bind(this)}
                     />
                 </CardSection>
             </Card>
@@ -48,7 +53,8 @@ const mapStatetoProps = state => {
     return{
         email: state.auth.email,
         password: state.auth.password,
+        user: state.auth.user
     };
 };
 
-export default connect(mapStatetoProps, { emailChanged, passwordChanged } )(LoginForm);
+export default connect(mapStatetoProps, { emailChanged, passwordChanged, loginUser } )(LoginForm);
