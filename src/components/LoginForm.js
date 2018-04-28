fa0082958d456cf2065b79bfef39c4561b412c55
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-04-28 18:53:38 +0530

    Add state change listener to email and password fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,36 @@
 import React , {Component} from 'react';
 import { Card, Button, CardSection, Input } from './common';
 import { connect } from 'react-redux';
+import { emailChanged, passwordChanged } from '../actions';
 
 class LoginForm extends Component {
+    onEmailChange(text) {
+        this.props.emailChanged(text);
+    }
+
+    onPasswordChange(text) {
+        this.props.passwordChanged(text);
+    }
+
 
     render() {
         return (
             <Card>
                 <CardSection>
                     <Input
+                        onChangeText={this.onEmailChange.bind(this)}
+                        value={this.props.email}
                         placeholder="something@gmail.com"
                         label="Email"
                     />
                 </CardSection>
                 <CardSection>
                     <Input
+                        onChangeText={this.onPasswordChange.bind(this)}
                         secureTextEntry
                         placeholder="password"
                         label="Password"
+                        value={this.props.password}
                     />
                 </CardSection>
 
@@ -31,4 +44,11 @@ class LoginForm extends Component {
     }
 };
 
-export default LoginForm;
+const mapStatetoProps = state => {
+    return{
+        email: state.auth.email,
+        password: state.auth.password,
+    };
+};
+
+export default connect(mapStatetoProps, { emailChanged, passwordChanged } )(LoginForm);
