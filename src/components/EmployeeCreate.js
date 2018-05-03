8536f587bd24627170e65b33238926c2901d10ac
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-05-03 21:38:39 +0530

    Update EmployeeCreate component to handle inputs and save state

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Button, Input} from './common'
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
     render() {
         return(
-            <View>
-                <Text> Hi </Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label='Name'
+                        placeholder='Dipansh'
+                        value={this.props.name}
+                        onChangeText={ text => this.props.employeeUpdate({prop: 'name', value: text}) }
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        label='Phone'
+                        placeholder='555-555-5555'
+                        value={this.props.phone}
+                        onChangeText={ text => this.props.employeeUpdate({prop: 'phone', value: text}) }
+                    />
+                </CardSection>
+                <CardSection>
+                    
+                </CardSection>
+                <CardSection>
+                    <Button
+                        text='Create'
+                    />
+                </CardSection>
+            </Card>
         );
     }
 }
 
-export default EmployeeCreate;
+const mapStatetoProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+    return { name, phone, shift };
+};
+
+export default connect(mapStatetoProps, { employeeUpdate } )(EmployeeCreate);
