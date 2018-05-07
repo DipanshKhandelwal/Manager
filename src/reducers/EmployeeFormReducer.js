7a7ec48a1796cbd75f7d3a83d019218ad5c3f05d
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-05-07 12:07:45 +0530

    Add Employee Save action

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,4 @@
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from '../actions/types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_SAVE_SUCCESS } from '../actions/types';
 
 const INITIAL_STATE = {
     name: '',
@@ -13,6 +13,10 @@ export default (state=INITIAL_STATE, action) => {
             return {...state, [action.payload.prop]: action.payload.value };
 
         case EMPLOYEE_CREATE:
+            // action.payload === { prop: 'name', value: 'Dipansh' }
+            return INITIAL_STATE;
+
+        case EMPLOYEE_SAVE_SUCCESS:
             // action.payload === { prop: 'name', value: 'Dipansh' }
             return INITIAL_STATE;
 
