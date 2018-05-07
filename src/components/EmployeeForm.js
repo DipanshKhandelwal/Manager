34588aede6bf6315bd510f3412e6af3b5bb7b41b
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-05-07 16:15:35 +0530

    Used Confirm class to confirm before deleting employee and delete on yes

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -55,7 +55,7 @@ const styles = {
     }
 }
 
-const mapStatetoProps = (state) => {
+const mapStateToProps = (state) => {
     const { name, phone, shift } = state.employeeForm;
     return { name, phone, shift };
 };
